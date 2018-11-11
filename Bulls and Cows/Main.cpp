6b37aa323673a5c7fc61dda4730857205a1a2ed2
Emilijus <slibinas2012@gmail.com>
2018-11-11 16:53:16 +0200

    ReLearn 01

--- Bulls and Cows/Main.cpp
+++ Bulls and Cows/Main.cpp
@@ -1,1 +1,34 @@
-// Test
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void PrintIntro();
+string GetGuessAndPrintBack();
+
+// Entry point for application
+int main()
+{
+	PrintIntro();
+	GetGuessAndPrintBack();
+	cout << endl;
+	return 0;
+}
+
+void PrintIntro()
+{
+	constexpr int WORD_LENGTH = 5;
+	cout << "Welcom to Bulls and Cows.\n";
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
+	cout << endl;
+	return;
+}
+
+string GetGuessAndPrintBack()
+{
+	cout << "Enter your guess: ";
+	string Guess = "";
+	getline(cin, Guess);
+	cout << "Your guess was: " << Guess << endl;
+	return Guess;
+}
